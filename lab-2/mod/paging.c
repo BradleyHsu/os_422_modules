893cf41eed65d88d41eabe46bfdd9a6146331760
Bradley Hsu <b.hsu@wustl.edu>
2023-04-10 03:13:48 -0500

    s

--- lab-2/mod/paging.c
+++ lab-2/mod/paging.c
@@ -82,7 +82,7 @@ void handle_close(struct vm_area_struct *vma) {
 }
 
 void append_new_address(struct vm_area_struct *vma, struct page *page) {
-    printk(KERN_INFO "Appending new address\n")
+    printk(KERN_INFO "Appending new address\n");
     struct vma_private *vma_priv = get_vma_private(vma);
     printk(KERN_INFO "Appending new address:\n");
     printk(KERN_INFO "Appending new address: %d\n", vma_priv->num_pages);
